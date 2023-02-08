ab2e953c5b50ad9b363c6f84f9bf13c6fbf78bdf
John Wood <johna613@gmail.com>
2023-02-07 20:08:20 -0500

    adding files

---
+++ app.js
@@ -0,0 +1,31 @@
+// console.log("Let's get this party started!");
+
+
+async function getGif (q){
+	const res = await axios.get('http://api.giphy.com/v1/gifs/search', { params:
+	{ api_key: 'r8mZlYrWJarkeAvnYusshn8LEsTfaDP4' , q} 
+	} );
+	addGif(res.data.data[Math.floor(Math.random()* 51)].images.original.url);
+}
+
+function addGif(url){
+	const newGif = document.createElement('img');
+	newGif.setAttribute('src', url);
+	const gifContainer = document.getElementById('gif-container');
+	gifContainer.append(newGif)
+}
+
+function removeGifs(){
+	$('#gif-container').empty()
+}
+
+$('#btn-search').on('click', function(event){
+	event.preventDefault()
+	const srchTerm = $('#srch-term').val();
+	getGif(srchTerm)
+	$('#srch-term').val('');
+})
+
+$('#btn-remove').on('clikc', removeGifs())
+
+
